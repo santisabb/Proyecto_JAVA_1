0ed6170e30f55dbc896daa94edd98c29fa10d0a8
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-06-08 20:19:33 -0300

    usando el metodo stream con foreach para aprovechar la funcionalidad

--- src/AltasFunciones.java
+++ src/AltasFunciones.java
@@ -5,7 +5,11 @@ public class AltasFunciones {
     private Function<Integer,Integer> suma = x -> x.sum(x,x);
 
     public void probandoooo(){
-        System.out.println(converMayus.apply("soy santi"));
+       String frase = Holis(converMayus , "soy santi");
+        System.out.println(frase);
         System.out.println(suma.apply(22));
+    }
+    public String Holis (Function<String,String> unaFuncion , String nombre){
+       return unaFuncion.apply("Hooliiiis, " + nombre);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         AltasFunciones af = new AltasFunciones();
         af.probandoooo();
+        ArrayList<String> nombres = new ArrayList<String>();
+        nombres.add("Santi");
+        nombres.add("Gustavo");
+        nombres.add("Maria");
+        nombres.add("Gordi");
+        nombres.stream().forEach(System.out::println);
+        //otra forma valida de hacer lo anterior es:
+        nombres.stream().forEach( x -> System.out.println(x));
     }
 }
